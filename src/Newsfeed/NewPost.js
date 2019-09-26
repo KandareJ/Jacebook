dd4164abb72beab4738d25a31c19c2c6b3e91191
KandareJ <jace.kandare@yahoo.com>
2019-09-25 19:39:28 -0600

    finished create post

--- src/Newsfeed/NewPost.js
+++ src/Newsfeed/NewPost.js
@@ -10,16 +10,18 @@ export default class NewPost extends Component {
       <div>
         <Card>
           <div className="new-content-header">Create Post</div>
-          <Grid container spacing={3}>
-            <Grid item xs={2} >
-              <img className="thumbnail-image" src="https://scontent-lax3-1.xx.fbcdn.net/v/t31.0-8/20229865_10213586156551604_8464216565975150589_o.jpg?_nc_cat=102&_nc_oc=AQlgXNJsxk9SXLcFjwKqPmypq4PgPZiIqxoBGbmVtUHkcdb6vX2K31OSds80oKlGSXg&_nc_ht=scontent-lax3-1.xx&oh=d13cce585230496816450947a5a7dc92&oe=5DF932A0" />
+          <div className="card-padding">
+            <Grid container spacing={3}>
+              <Grid item xs={2} >
+                <img className="thumbnail-image" src="https://scontent-lax3-1.xx.fbcdn.net/v/t31.0-8/20229865_10213586156551604_8464216565975150589_o.jpg?_nc_cat=102&_nc_oc=AQlgXNJsxk9SXLcFjwKqPmypq4PgPZiIqxoBGbmVtUHkcdb6vX2K31OSds80oKlGSXg&_nc_ht=scontent-lax3-1.xx&oh=d13cce585230496816450947a5a7dc92&oe=5DF932A0" />
+              </Grid>
+              <Grid item xs={10}>
+                <textarea className="new-post-content" placeholder="What's on your mind?"></textarea>
+              </Grid>
             </Grid>
-            <Grid item xs={10}>
-              <input type="text" className="new-post-content" placeholder="What's on your mind?"></input>
-            </Grid>
-          </Grid>
-          <hr className="divider" />
-          <NewPostButtons />
+            <hr className="divider" />
+            <NewPostButtons />
+          </div>
         </Card>
       </div>
     );
--- src/Newsfeed/NewPostButtons.js
+++ src/Newsfeed/NewPostButtons.js
@@ -1,27 +1,29 @@
 import React, { Component } from 'react';
 import { Grid } from '@material-ui/core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faImage, faVideo } from '@fortawesome/free-solid-svg-icons';
+import { faPhotoVideo, faUser, faHashtag } from '@fortawesome/free-solid-svg-icons';
 import './NewsFeed.css';
 
 export default class NewPostButtons extends Component {
   render() {
     return(
-      <div>
-        <Grid container spacing={3}>
-          <Grid item xs={4}>
-            <button className="new-content-media">
-              <FontAwesomeIcon className="icon-color" size="x" icon={faImage} />
-              <div>Add Image</div>
-            </button>
-          </Grid>
-          <Grid item xs={4}>
-            <FontAwesomeIcon className="new-content-media" size="x" icon={faVideo} />
-          </Grid>
-          <Grid item xs={4}>
-            <button>Create</button>
-          </Grid>
-        </Grid>
+      <div className="button-inline">
+        <div className="new-post-option">
+          <input className="add-media-hidden" id="postImage" type="file" name="media" accept="image/*, video/*" />
+          <label for="postImage" className="button-inline">
+            <FontAwesomeIcon className="icon-color" size="x" icon={faPhotoVideo} /><div className="new-post-option-text">Add Image/Video</div>
+          </label>
+        </div>
+
+        <div className="new-post-option button-inline">
+            <FontAwesomeIcon className="icon-color" size="x" icon={faUser} /><div className="new-post-option-text">Tag a friend</div>
+        </div>
+
+        <div className="new-post-option button-inline">
+            <FontAwesomeIcon className="icon-color" size="x" icon={faHashtag} /><div className="new-post-option-text">Hashtag</div>
+        </div>
+
+        <button className="post-button">Post</button>
       </div>
     );
   }
